c56bf1d151eb2ea888483e64ade210ce03bfc70c
BorowskiKamil <kb.kamil.borowski@gmail.com>
2019-10-22 18:44:02 +0200

    Migrations for categories service

---
+++ Adsboard.Services.Categories/src/Adsboard.Services.Categories/Migrations/20191022164320_CategoryEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Adsboard.Services.Categories.Migrations
+{
+    public partial class CategoryEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(nullable: false),
+                    name = table.Column<string>(type: "VARCHAR(255)", nullable: false),
+                    created_at = table.Column<DateTimeOffset>(nullable: false),
+                    creator_id = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "categories");
+        }
+    }
+}
